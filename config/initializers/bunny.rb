a757dd20310c7edde84ff6d90eb165de37464a3d
Scott Hamilton <shamil614@yahoo.com>
2015-06-01 16:14:54 -0500

    Setup bunny and connection for dev

---
+++ config/initializers/bunny.rb
@@ -0,0 +1,29 @@
+# RabbitMQ connection information
+
+# TODO Account for ENV specific connnections
+$bunny = Bunny.new
+$bunny.start
+
+$default_channel = $bunny.create_channel
+
+# Tracking process info
+puts "Rails Process ID: #{Process.pid}"
+
+# It appears that bunny gem handles the connection closing / stopping properly.
+
+# Handle Ctrl-C
+Signal.trap("INT") do
+  puts "INT PUTS Little bunny foo foo is a Goon....closing connection to RabbitMQ"
+end
+
+# Handle Hangup
+Signal.trap("HUP") do
+  puts "HUP PUTS Little bunny foo foo is a Goon....closing connection to RabbitMQ"
+end
+
+# Handles `Kill` signals
+Signal.trap("TERM") do
+  # Documentation says closing connection will first close the channels belonging to the connection
+  # Alternatively a channel can be closed independently ```channel.close```
+  puts "TERM PUTS Little bunny foo foo is a Goon....closing connection to RabbitMQ"
+end
